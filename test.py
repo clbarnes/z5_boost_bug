0a6ea714ec6f65ae59739785d654091cc76a3abb
Chris Barnes <barnesc@janelia.hhmi.org>
2018-04-09 13:41:44 -0400

    add loads of logging

--- test.py
+++ test.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env
 
-import z5py
+import os
+import logging
 from datetime import datetime
-fpath = datetime.now().isoformat() + '.n5'
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+logger.info('importing z5py')
 
-f = z5py.File(fpath, use_zarr_format=False)
+import z5py
+logger.info('imported z5py')
+
+file_path = datetime.now().isoformat() + '.n5'
+
+logger.info('creating File')
+f = z5py.File(file_path, use_zarr_format=False)
+logger.info('created File')
+assert os.path.isdir(file_path), "z5py did not create dir at " + file_path
+
+logger.info('creating Dataset')
+ds = f.create_dataset('ds', shape=(10, 10, 10), dtype=np.dtype('uint8'), chunks=(1, 10, 10))
+logger.info('created Dataset')
+ds_path = os.path.join(file_path, 'ds')
+assert os.path.isdir(ds_path), "z5py did not create dir at " + ds_path
 
