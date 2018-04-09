f716557eb92513611b06c30314ddb34ede9f5b2f
Chris Barnes <barnesc@janelia.hhmi.org>
2018-04-09 13:50:29 -0400

    remove np reference

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ logger.info('created File')
 assert os.path.isdir(file_path), "z5py did not create dir at " + file_path
 
 logger.info('creating Dataset')
-ds = f.create_dataset('ds', shape=(10, 10, 10), dtype=np.dtype('uint8'), chunks=(1, 10, 10))
+ds = f.create_dataset('ds', shape=(10, 10, 10), dtype='uint8', chunks=(1, 10, 10))
 logger.info('created Dataset')
 ds_path = os.path.join(file_path, 'ds')
 assert os.path.isdir(ds_path), "z5py did not create dir at " + ds_path
